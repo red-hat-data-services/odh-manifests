745eb097d32a177ee20c0b3980e5431a89a96256
jooho <jlee@redhat.com>
2021-03-23 14:03:49 -0400

    update jhtest_load.py to use common methods from jhtest.py for future update

--- tests/basictests/jupyterhub/jhtest_load.py
+++ tests/basictests/jupyterhub/jhtest_load.py
@@ -18,6 +18,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.select import Select
 
 from selenium.common.exceptions import NoSuchElementException
+from jhtest import JHStress
 
 logging.basicConfig(level=logging.INFO)
 _LOGGER = logging.getLogger()
@@ -29,7 +30,7 @@ class Del:
   def __getitem__(self, k):
     return self.comp.get(k)
 
-class JHStress():
+class JHLoad():
     def __init__(self):
         self.load_config()
         chrome_options = Options()  
@@ -45,6 +46,10 @@ class JHStress():
         _LOGGER.info("Using JupyterHub URL %s" % self.url)
         self.action = ActionChains(self.driver)
         _LOGGER.info("Config loaded, driver initialized")
+        self.jhs = JHStress()        
+        self.jhs.as_admin=self.as_admin
+        self.jhs.preload_repos=self.preload_repos
+        
 
     def load_config(self):
 
@@ -73,24 +78,13 @@ class JHStress():
             _LOGGER.error("You need to provide $JH_URL env var.")
             raise Exception("You need to provide $JH_URL env var.")
 
-    def click_menu(self, button):
-        cell_elem = self.driver.find_element(By.XPATH, '//a[text()="%s"]' % button)
-        cell_elem.click()
-
-    def openshift_login(self, username, password):
-        username_elem = self.driver.find_element_by_id("inputUsername")
-        username_elem.send_keys(username)
-        password_elem = self.driver.find_element_by_id("inputPassword")
-        password_elem.send_keys(password)
-        login_elem = self.driver.find_element(By.XPATH, '//button[text()="Log in"]')
-        login_elem.send_keys(Keys.RETURN)
-
     def run(self):
         try:
-            self.login()
+            self.jhs.login()
             _LOGGER.info("Logged in")
             
-            self.spawn()
+            self.jhs.spawn()
+            self.driver = self.jhs.driver
             _LOGGER.info("Spawned the server")
             tab = self.driver.window_handles[0]
 
@@ -100,101 +94,23 @@ class JHStress():
                 _LOGGER.info("Notebook %s finished" % notebook)
 
             _LOGGER.info("Report Metrics to PushGateWay")
-            # This is commented because there is no ways to access internal pushgateway from OSD.
-            # But as soon as we find the way, this will be uncommented. 
             self.report_metrics()
-            self.stop()
+            self.jhs.stop()
             _LOGGER.info("Stopped the server")
         except Exception as e:
             _LOGGER.error(e)
             self.driver.get_screenshot_as_file(os.path.join(_SCREENSHOT_DIR, "exception.png"))
             raise e
 
-    def deal_with_privacy_error(self):
-        if "Privacy error" in self.driver.title:
-            elem = self.driver.find_element_by_id("details-button")
-            elem.send_keys(Keys.RETURN)
-            elem = self.driver.find_element_by_id("proceed-link")
-            elem.send_keys(Keys.RETURN)
-        
-    def login(self):
-        self.deal_with_privacy_error()
-        elem = self.driver.find_element_by_link_text("Sign in with OpenShift")
-        elem.send_keys(Keys.RETURN)
-        self.deal_with_privacy_error()
-
-        if self.check_exists_by_xpath('//a[text()="%s"]' % self.login_provider):
-            elem = self.driver.find_element_by_link_text(self.login_provider)
-            elem.send_keys(Keys.RETURN)
-            self.openshift_login(self.username, self.password)
-
-        permissions_xpath = '//input[@value="Allow selected permissions"]'
-        if self.check_exists_by_xpath(permissions_xpath):
-            elem = spawn_elem = self.driver.find_element(By.XPATH, permissions_xpath)
-            elem.send_keys(Keys.RETURN)
-    
-    def test_environment_variables(self):
-        _LOGGER.info("Testing Environment Variables")
-        self.add_environment_variable('foo', 'bar')
-        self.add_environment_variable('foo1', 'bar1')
-        self.remove_last_environment_variable()
-        self.remove_last_environment_variable()
-    
-    def add_environment_variable(self, key, value):
-        self.driver.implicitly_wait(10)
-        add_button_elem = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/header/form/form/button')
-        add_button_elem.send_keys(Keys.RETURN)
-        key_elem = self.driver.find_element(By.NAME, 'variable_name')
-        value_elem = self.driver.find_element(By.NAME, 'variable_value')
-        key_elem.clear()
-        key_elem.send_keys(key)
-        value_elem.clear()
-        value_elem.send_keys(value)
-
-    def remove_last_environment_variable(self):
-        remove_button_elems = self.driver.find_elements(By.CLASS_NAME, 'btn-danger')
-        elem = remove_button_elems[len(remove_button_elems) - 1]
-        elem.send_keys(Keys.RETURN)
-
-    def spawn(self):
-        self.driver.implicitly_wait(10)
-        image_drop = self.driver.find_element(By.XPATH, '//*[@id="ImageDropdownBtn"]')
-        image_drop.send_keys(Keys.RETURN)
-
-        self.driver.implicitly_wait(10)
-        image_select = self.driver.find_element_by_id(self.spawner["image"])
-        image_select.click()
-
-        size_drop = self.driver.find_element(By.XPATH, '//*[@id="SizeDropdownBtn"]')
-        size_drop.click()
-
-        size_select = self.driver.find_element_by_id(self.spawner["size"])
-        size_select.click()
-
-        gpu_elem = self.driver.find_element(By.XPATH, '//*[@id="gpu-form"]')
-        gpu_elem.clear()
-        gpu_elem.send_keys(self.spawner['gpu'])
-
-        self.test_environment_variables()
-
-        time.sleep(0.5)
-
-        self.add_environment_variable("JUPYTER_PRELOAD_REPOS", self.preload_repos)
-
-        gpu_elem.send_keys(Keys.RETURN)
-
-    def stop(self):
-        self.driver.get(self.url+ "/hub/home")
-        stop_elem = self.driver.find_element_by_id("stop")
-        stop_elem.click()
 
     def run_notebook(self, notebook, tab):
-        _LOGGER.info(self.driver.current_url)
+        _LOGGER.info("Current URL:"+ self.driver.current_url)
         path = notebook.split("/")
      
-        w = WebDriverWait(self.driver, 300)      
-
+        w = WebDriverWait(self.driver, 100)      
+        _LOGGER.info("Current URL:"+ self.driver.current_url)
         auth_elem = w.until(EC.presence_of_element_located((By.XPATH, '//div[@id="notebook_list"]')))
+        # auth_elem = w.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/form/input')))
         auth_elem.click()
         
         framework=""
@@ -214,7 +130,7 @@ class JHStress():
         #Switch to new tab
         self.driver.switch_to_window(self.driver.window_handles[1])
 
-        self.run_all_cells()
+        self.jhs.run_all_cells()
 
         output_data = w.until(EC.presence_of_all_elements_located((By.XPATH,'//div[@id="notebook-container"]/div[2]/div[2]//pre')))
 
@@ -229,51 +145,8 @@ class JHStress():
                 _LOGGER.info(self.driver.current_url)
 
   
-    def run_all_cells(self):
-        try:
-            w = WebDriverWait(self.driver, 30)
-            element = w.until(EC.presence_of_element_located((By.XPATH, '//i[@id="kernel_indicator_icon"][@title="Kernel Idle"]')))
-        except Exception as e:
-            _LOGGER.error(e)
-            raise e
-
-        wait = WebDriverWait(self.driver, 10)
-        elem = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[text()="Cell"]')))
-
-        self.click_menu("Cell")
-        elem = self.driver.find_element_by_id("all_outputs")
-        self.driver.execute_script("arguments[0].setAttribute('class','dropdown-submenu open')", elem)
-
-        clear_elem = self.driver.find_element_by_id("clear_all_output")
-        clear_elem.click()
-
-        self.click_menu("Cell")
-        self.click_menu("Run All")
-
-        cells = self.driver.find_elements(By.XPATH, '//div[contains(@class, "cell code_cell")]')
-
-        last = len(cells)
-        wait_time = 600
-        retries = int(wait_time/2)
-        last_cell = None
-        for i in range(0, retries):
-            last_cell = self.driver.find_element(By.XPATH, '(//div[contains(@class, "cell code_cell")])[%d]//div[@class="prompt_container"]' % last)
-            if last_cell.text == "In [*]:" or last_cell.text == "In [ ]:":
-                time.sleep(2)
-            else:
-                break
-        time.sleep(2)
-        _LOGGER.info("Reached last cell, execution numbering: '%s'" % last_cell.text)
-
     def quit(self):
         self.driver.quit()
-
-    def check_exists_by_xpath(self, xpath):
-        try:
-            self.driver.find_element_by_xpath(xpath)
-        except NoSuchElementException:
-            return False
-        return True
 
     def extract_result(self,selected_element,framework):
         _LOGGER.info("in push data")
@@ -332,6 +205,6 @@ class JHStress():
         self.step_gauge.labels("mnist-minimal",framework,current_time,epoch_num).set(step_value)
 
 if __name__ == "__main__":
-    jhs = JHStress()
-    jhs.run()
-    jhs.quit()
+    jhl = JHLoad()
+    jhl.run()
+    jhl.quit()
